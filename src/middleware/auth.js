420e9c5c5e89fd06203c26e61ffa56ec581d6368
bhargav <bhargavmistry4444@gmail.com>
2023-12-29 16:41:03 +0530

    jwt secret key get way change

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,8 @@ const User = require("../models/user.model");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const decoded = jwt.verify(token, "c7d6cf0ec2d268fd892e4d1a94322c16c4626f6f512eda28e811b3cc9e58a033d09bc331ddb6f3748a61c13881f1b042");
     const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
 
     if (!user) {
